b4db40283f683087bb891a46812080ec466349aa
Ruanito Santos <ruanitodiego@gmail.com>
2017-01-15 20:04:52 -0200

    Create log file and folder

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 */
 var express           = require('express');
 var minimist          = require('minimist');
+var morgan            = require('morgan');
+var fs                = require('fs');
+var FileStreamRotator = require('file-stream-rotator');
 
 /*
 * Create Express app
@@ -26,6 +29,18 @@ if (argv.help || argv.h) {
 	return 0;
 }
 
+/*
+* Crete log directory and file
+*/
+var logDirectory = __dirname + '/log';
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+var accessLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false
+});
+
 var port = argv.p || argv.port || 8080;
 
 /*
@@ -35,6 +50,10 @@ app.listen(port, function() {
 	console.log('Server is running on port:'+port);
 });
 
+/*
+* Write in log file
+*/
+app.use(morgan('combined', {stream: accessLogStream}));
 
 /*
 * Use web folder to show index
