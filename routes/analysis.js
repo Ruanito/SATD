180d9a881109b73573addb6caa143d8bbabbb133
Ruanito Santos <ruanitodiego@gmail.com>
2017-01-16 20:04:27 -0200

    Search into database keyword

--- routes/analysis.js
+++ routes/analysis.js
@@ -1,5 +1,23 @@
+/*
+* Search keyword in the database, if not found insert into
+*/
 exports.search = function (req, res) {
 	var sKeyWord = req.params.keyWord;
-	
-	res.json({status: 'sucess', message: sKeyWord});
+
+	db.query("SELECT * FROM searchs WHERE keyword like ?", [sKeyWord], function (err, results, fields){
+		if (err)
+			throw err;
+		else {
+			if (results.length === 0) {
+				db.query("INSERT INTO searchs (keyword, date) VALUES (?, now())", [sKeyWord], function(err, result) {
+					if (err)
+						throw err;
+					return res.json({status: "sucess", message: result});
+				});
+			} else {
+				return res.json({status: "sucess", message: results});
+			}
+		}
+	});
+
 }
